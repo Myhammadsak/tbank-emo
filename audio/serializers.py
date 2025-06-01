872ee36b30c94a3a2e04a24f0dbb11b5c85a426c
Холмухаммад <143716091+Myhammadsak@users.noreply.github.com>
2025-06-01 17:19:37 +0700

    Delete audio directory

--- audio/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Audio
-
-
-class AudioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Audio
-        fields = '__all__'
-        read_only_fields = ('grade', 'created_at', 'user')
--- audio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.urls import path
-from .views import AudioViewSet
-
-app_name = 'audio'
-
-urlpatterns = [
-    path('audio/', AudioViewSet.as_view({'post': 'create'}), name='audio-analysis'),
-]
